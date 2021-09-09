3e7212513312f5a56a2ce604e1db9421dfdd3ece
Park <rppr001@naver.com>
2021-09-09 23:02:03 +0900

    javascript chrome app usrname commit

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
 
-function onLoginBtnClick(){
-   console.log(username);
 
+
+const hidden_classname = "hidden";
+
+const usrname_KEy = "username";
+
+function onLoginsubmit(event){
+    event.preventDefault();
+    loginForm.classList.add(hidden_classname);
+    const username = loginInput.value;
+    localStorage.setItem("usrname_KEy", username)
+   paintGreetings(username);
 }
 
-loginButton.addEventListener("click",onLoginBtnClick);
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(hidden_classname);
+}
+
+
+const savedUsername = localStorage.getItem("usrname_KEy");
+
+if(savedUsername === null){
+    // form 보여주기
+    loginForm.classList.remove(hidden_classname);
+    loginForm.addEventListener("submit", onLoginsubmit);
+}else{
+   paintGreetings(savedUsername);
+}
