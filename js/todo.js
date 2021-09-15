d397bd14775f8107204e1dfe0e889ef33d7a5c7f
Park <rppr001@naver.com>
2021-09-15 21:59:52 +0900

    javascript todo-list delete event targer parentElement commit

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,21 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function deletebutton(event){
+    const li = event.target.parentElement;
+    li.remove();
+
+}
+
 function paintTodo(newTodo){
     const li = document.createElement("li"); //tag 생성
     const span = document.createElement("span");
-    li.appendChild(span); //span을 li안에 넣겠다.
     span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deletebutton);
+    li.appendChild(span); //span을 li안에 넣겠다.
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
